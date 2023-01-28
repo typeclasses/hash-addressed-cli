2d7384bcef17ad591ae1941fa34186d718d21eba
Chris Martin <ch.martin@gmail.com>
2023-01-28 15:52:31 -0700

    comments on verbosity

--- hash-addressed-cli/library/HashAddressed/App/Verbosity/Printing.hs
+++ hash-addressed-cli/library/HashAddressed/App/Verbosity/Printing.hs
@@ -14,11 +14,21 @@ import Prelude (String)
 
 import qualified System.IO as IO
 
+{-| For output that one might want to use programmatically
+
+    Goes to stdout.
+
+    Can be suppressed with the @--quiet@ flag. -}
 putNormalLn :: MonadIO m => Verbosity -> String -> m ()
 putNormalLn Verbosity{ quiet } x =
     if quiet then pure () else liftIO $ IO.hPutStrLn IO.stdout x
 
+{-| For extra chatty messages
+
+    Goes to stderr so that the output that one might want to use
+    programmatically can be captured separately.
+
+    Does not print unless the @--verbose@ flag is used. -}
 putVerboseLn :: MonadIO m => Verbosity -> String -> m ()
 putVerboseLn Verbosity{ quiet, verbose } x =
     if quiet || not verbose then pure () else liftIO $ IO.hPutStrLn IO.stderr x
-
